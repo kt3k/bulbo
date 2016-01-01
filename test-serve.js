f12210065025d2c7952d6a6419d1b646760ffa8a
Yoshiya Hinosawa <stibium121@gmail.com>
2016-01-01 14:08:58 +0900

    Use chokidar for watching

---
+++ test-serve.js
@@ -0,0 +1,20 @@
+var bulbo = require('./')
+var through = require('through')
+var browserify = require('browserify')
+
+var asset = bulbo.asset
+var dest = bulbo.dest
+
+asset('test/fixture/**/*.js')(function (src) {
+
+    return src.pipe(through(function (file) {
+
+        file.contents = browserify(file.path).bundle()
+
+        this.queue(file)
+
+    }))
+
+})
+
+bulbo.serve()
