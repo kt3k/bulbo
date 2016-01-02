9d80d2ff00c50bef3baece9cfbd58d2031d26535
Yoshiya Hinosawa <stibium121@gmail.com>
2016-01-02 19:31:27 +0900

    Add test scaffolding

---
+++ spec/index.js
@@ -0,0 +1,23 @@
+'use strict'
+
+var bulbo = require('../')
+
+describe('moduleIF', function () {
+
+    beforeEach(function () {
+
+        bulbo.clear()
+
+    })
+
+    describe('asset', function () {
+
+        it('registers the asset', function () {
+        })
+
+        it('returns the setter of the asset\'s modifier', function () {
+        })
+
+    })
+
+})
