b210e9dc56aeac367ed6b7a047dafd8134a2404f
Yoshiya Hinosawa <stibium121@gmail.com>
2016-05-01 14:00:43 +0900

    Update demo

--- demo/bulbofile.js
+++ demo/bulbofile.js
@@ -2,9 +2,8 @@ var asset = require('../').asset
 var through2 = require('through2')
 var browserify = require('browserify')
 
-asset('../spec/fixture/**/*.js').build(function (src) {
-
-    return src.pipe(through2.obj((file, enc, cb) => browserify(file.path).bundle((err, contents) => {
+asset('../spec/fixture/**/*.js')
+.pipe(through2.obj((file, enc, cb) => browserify(file.path).bundle((err, contents) => {
 
         if (err) cb(err)
 
@@ -14,8 +13,6 @@ asset('../spec/fixture/**/*.js').build(function (src) {
             cb(null, file)
         }, 5)
 
-    })))
-
-})
+})))
 
 asset('../spec/fixture/**/*.css')
